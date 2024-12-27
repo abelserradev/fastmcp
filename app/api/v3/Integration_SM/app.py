675265f13501c81e707ba3e775188919be5131cb
Ernesto Crespo <ernesto@asistensi.com>
2024-12-27 10:18:54 -0400

    WIP

--- app/api/v3/Integration_SM/app.py
+++ app/api/v3/Integration_SM/app.py
@@ -20,7 +20,7 @@ from app.utils.v1.constants import (
 from app.utils.v1.LoggerSingleton import logger
 
 
-# from app.utils.v3.payload_templates import payload_cotizacion
+from app.utils.v3.payload_templates import payload_cotizacion
 
 router = APIRouter(
     tags=["MS Integration Version 3"],
@@ -39,37 +39,71 @@ async def crear_cotizacion(
     request: CrearPolizaBase
 ):
     data = request.model_dump(exclude_unset=True)
-#    payload = payload_cotizacion.copy()
-#     datos = payload.get("coll_datos").get("datos").copy()
-#
-#
-#     num_hijos = int(data.get("cantidad_hijos"))
-#     tiene_conyugue = data.get("tiene_conyugue")
-#     beneficiarios = data.get("beneficiarios", [])
-#
-#     if num_hijos > 0:
-#         datos.append({
-#             "cd_dato": "710003",  # Número de hijos.
-#             "nu_bien": "1",
-#             "valor": f"{num_hijos}"
-#         })
+    payload = payload_cotizacion.copy()
+    datos = payload.get("coll_datos").get("datos").copy()
 
-    # if tiene_conyugue:
-    #     fe_nac_conyugue = ""
-    #     for beneficiario in beneficiarios:
-    #         if beneficiario["cd_parentesco"] == "CONYUGUE"
-    #             fe_nac_conyugue = beneficiario["fe_nacimiento"]
-    #             break
+
+    num_hijos = int(data.get("cantidad_hijos"))
+    tiene_conyugue = data.get("tiene_conyugue")
+    beneficiarios = data.get("beneficiarios", [])
+
+    if num_hijos > 0:
+        datos.append({
+            "cd_dato": "710003",  # Número de hijos.
+            "nu_bien": "1",
+            "valor": f"{num_hijos}"
+        })
     #
-    #     datos.append(
-    #         {
-    #             "cd_dato": "710051",  # Fecha de nacimiento del conyuge. Se pasa si se tiene un conyugue vinculado.
-    #             "nu_bien": "1",
-    #             "valor": fe_nac_conyugue
-    #         }
-    #     )
+    if tiene_conyugue:
+        fe_nac_conyugue = ""
+        for beneficiario in beneficiarios:
+            parentesco = beneficiario.get("cd_parentesco").value
+            if parentesco == "CONYUGUE":
+                fe_nac_conyugue = beneficiario["fe_nacimiento"]
+                break
+
+        datos.append(
+            {
+                "cd_dato": "710051",  # Fecha de nacimiento del conyuge. Se pasa si se tiene un conyugue vinculado.
+                "nu_bien": "1",
+                "valor": fe_nac_conyugue
+            }
+        )
+
+    nombre_titular = f"{data['nm_primer_nombre']} {data['nm_primer_apellido']}"
+    dni_titular = data['documento']['nu_documento'].value
+    sexo_titular = data['sexo'].value
+    fecha_nacimiento_titular = data['fecha_nacimiento']
+    fe_desde = data['poliza']['fe_desde']
+    fe_hasta = data['poliza']['fe_hasta']
+    frecuencia = data['poliza']['frecuencia_cuota'].value
+    # Parentesco titular
+    datos.append(
+        {
+            "cd_dato": "710000",
+            "nu_bien": "1",
+            "valor": "1"
+        },
+    )
+    # Fecha nacimiento titular
+    datos.append(
+        {
+            "cd_dato": "710037",  # Fecha nacimiento títular. dd/mm/yyy
+            "nu_bien": "1",
+            "valor": fecha_nacimiento_titular
+        },
+    )
+    # Sexo titular
+    datos.append(
+        {
+            "cd_dato": "710036",  # Sexo del títular F/M.
+            "nu_bien": "1",
+            "valor":sexo_titular
+        }
+    )
 
 
 
-    logger.info(f"data: {data}")
-    return {"data": data}
+
+    logger.info(f"data: {datos}")
+    return {"data": datos}
