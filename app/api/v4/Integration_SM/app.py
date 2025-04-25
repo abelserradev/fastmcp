07259a2349bc8d4044b56b2c2ff81df711890fef
Ernesto Crespo <ecrespo@protonmail.com>
2025-04-25 14:01:17 -0400

    Fixed error

--- app/api/v4/Integration_SM/app.py
+++ app/api/v4/Integration_SM/app.py
@@ -219,9 +219,9 @@ def crear_cotizacion(
     payload["coll_generales"] = {"generales": [general]}
     payload["coll_grpaseg"] = {"grpaseg": grpasegs}
     try:
-        logger.info(f"URL: {url_cotizar}")
+        logger.info(f"URL-> {url_cotizar}")
         logger.info(f"Payload-> {json.dumps(payload)}")
-        logger.info(f: {headers})
+        logger.info(f"headers-> {headers}")
         response = sync_fetch_url(
             "POST",
             url_cotizar,
