b48203f5642319cf46b6e1ef34ac4f9a031faf16
Ernesto Crespo <ernesto@asistensi.com>
2025-05-30 08:36:51 -0400

    Simplify error handling by removing redundant JSON parsing logic.

--- app/api/v1/Integration_SM/app.py
+++ app/api/v1/Integration_SM/app.py
@@ -109,10 +109,10 @@ async def consultar_persona(
 
 
     if response.status_code != 200:
-        try:
-            detail = f"{response.json()['status']['code']} {response.json()['status']['descripcion']}"
-        except KeyError:
-            detail = f"{response.text}"
+        # try:
+        #     detail = f"{response.json()['status']['code']} {response.json()['status']['descripcion']}"
+        # except KeyError:
+        detail = f"{response.text}"
         logger.error(detail)
         raise HTTPException(status_code=response.status_code,
                             detail=detail)
