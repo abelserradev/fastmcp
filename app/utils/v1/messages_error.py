b6e9c6ce202f1024e5e6a5d4c02c9a4c3817ec53
Ernesto Crespo <ernesto@asistensi.com>
2025-07-30 14:04:38 -0400

    Centralize error messages in `messages_error` and update routes to use new constants for improved maintainability. Add validation for `cd_recibo` in payment and notification endpoints.

--- app/utils/v1/messages_error.py
+++ app/utils/v1/messages_error.py
@@ -1,3 +1,6 @@
 
 INTERNAL_ERROR = "Error interno del servidor"
 TIMEOUT_ERROR = "Tiempo de espera excedido"
+TIPO_PAGO_ERROR = "Tipo de pago tiene que ser USD o BS"
+CD_RECIBO_ERROR = "No se puede registrar notificaciones de pago con cd_recibo en cero"
+TIPO_INSTRUMENTO_ERROR = "Tipo de instrumento no es C2P,TDC o TDD"
